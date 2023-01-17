489845afad1d1f3aa28d1740d8ef81d0efd4a169
achiko06 <achiko06@gmail.com>
2023-01-17 16:45:17 +0100

    goauth and zustand store

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,29 +4,29 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im';
-import Discover from './Discover';
+
 import SuggestedAccounts from './SuggestedAccounts';
+import Discover from './Discover';
 import Footer from './Footer';
+import useAuthStore from '../store/authStore';
 
 const Sidebar: NextPage = () => {
   const [showSidebar, setShowSidebar] = useState<Boolean>(true);
   const { pathname } = useRouter();
+  const { fetchAllUsers, allUsers }: any = useAuthStore();
 
-  const userProfile = false;
-
-  const activeLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#0e11bd] rounded';
+  const activeLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#36e014] rounded';
 
   const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold rounded';
 
   return (
     <div>
-      <div 
+      <div
         className='block xl:hidden m-2 ml-4 mt-3 text-xl'
-        onClick={() => setShowSidebar((prev) => !prev)}
+        onClick={() => setShowSidebar(!showSidebar)}
       >
         {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
       </div>
-
       {showSidebar && (
         <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3 '>
           <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
@@ -41,25 +41,17 @@ const Sidebar: NextPage = () => {
               </div>
             </Link>
           </div>
-          {!userProfile && (
-            <div className='px-2 py-4 hidden xl:block'>
-              <p className='text-gray-400'>Log in to like and comment on videos</p>
-              <div className='pr-4'>
-                Google login
-              </div>
-            </div>
-          )}
-
+          
           <Discover />
-          <SuggestedAccounts />
+          <SuggestedAccounts
+            fetchAllUsers={fetchAllUsers}
+            allUsers={allUsers}
+          />
           <Footer />
         </div>
-        
       )}
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
